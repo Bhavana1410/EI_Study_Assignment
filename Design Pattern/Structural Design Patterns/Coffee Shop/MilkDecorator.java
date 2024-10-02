215be4d44c3f57a688fa2cfda3f48d8e8ba5d13f
Bhavana1410 <99717344+Bhavana1410@users.noreply.github.com>
2024-10-02 18:23:08 +0530

    Create MilkDecorator.java

---
+++ Design Pattern/Structural Design Patterns/Coffee Shop/MilkDecorator.java
@@ -0,0 +1,17 @@
+public class MilkDecorator implements Coffee {
+    private Coffee coffee;
+
+    public MilkDecorator(Coffee coffee) {
+        this.coffee = coffee;
+    }
+
+    @Override
+    public String getDescription() {
+        return coffee.getDescription() + ", Milk";
+    }
+
+    @Override
+    public double getCost() {
+        return coffee.getCost() + 1.5;
+    }
+}
