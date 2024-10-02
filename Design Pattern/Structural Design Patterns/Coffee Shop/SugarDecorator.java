c7f4ffcf7cba5872d4e05bf7c246967b2416161d
Bhavana1410 <99717344+Bhavana1410@users.noreply.github.com>
2024-10-02 18:23:28 +0530

    Create SugarDecorator.java

---
+++ Design Pattern/Structural Design Patterns/Coffee Shop/SugarDecorator.java
@@ -0,0 +1,17 @@
+public class SugarDecorator implements Coffee {
+    private Coffee coffee;
+
+    public SugarDecorator(Coffee coffee) {
+        this.coffee = coffee;
+    }
+
+    @Override
+    public String getDescription() {
+        return coffee.getDescription() + ", Sugar";
+    }
+
+    @Override
+    public double getCost() {
+        return coffee.getCost() + 0.5;
+    }
+}
